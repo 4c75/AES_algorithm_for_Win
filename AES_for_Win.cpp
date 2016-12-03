e40aeadc669d6a47bbdd3915fee57d6eebbf211e
4c75 <lukass.dzalbs@inbox.lv>
2016-12-03 21:24:02 +0200

    make new

---
+++ AES_for_Win.cpp
@@ -0,0 +1,15 @@
+// AES_for_Win.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	char r;
+	cin >> r;
+	return 0;
+}
+
